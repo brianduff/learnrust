505dfdcf84b1df34aaf0ee9034f50288595931d5
Brian Duff <bduff@fb.com>
2019-11-27 13:16:05 -0800

    7.5 Separating modules into separate files

---
+++ restaurant2/src/lib.rs
@@ -0,0 +1,10 @@
+// Rust loads the module from another file named "front_of_house.rs"
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
