da7ae4ea3c6030c1fed357103204ae7666cd4558
Brian Duff <bduff@fb.com>
2019-11-28 20:53:43 -0800

    12.2 Reading a File

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {} in {}", query, filename);
+    let contents = fs::read_to_string(filename)
+        .expect("Couldn't read file");
+
+    println!("{}", contents);
 }
